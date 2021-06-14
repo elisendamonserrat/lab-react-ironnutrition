27aa9a443688f538b52684ec267eba0ff181026d
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-14 15:38:57 +0200

    creates addFood component and changes App to a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import FoodBox from './components/FoodBox';
 import foods from './foods.json';
+import AddFoodInput from './components/AddFoodInput';
 
 
-function App() {
-  return (
-    <div className="App">
-      {foods.map((food, index) => {
-        return(
-          < FoodBox 
-              key={index}
-              food={food}
-              id={index}
-          />
-        )
-      })
-      }
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  addFood = () => {
+    console.log('update state and add new food to the array')
+  }
+
+  render() {
+    return (
+      <div className="App text-center">
+        <h1 className="title is-1">IronNutrition</h1>
+        < AddFoodInput onAddFood={this.addFood} />
+        {foods.map((food, index) => {
+          return(
+            < FoodBox 
+                key={index}
+                food={food}
+                id={index}
+            />
+          )
+        })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
